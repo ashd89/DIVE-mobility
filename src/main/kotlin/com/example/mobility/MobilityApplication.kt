e9161c060df54d464d53d2d3ee6ed23cadbe8981
ashd89 <jyjhlee0809@gmail.com>
2025-08-11 23:01:59 +0900

    feat: springboot project basic

---
+++ src/main/kotlin/com/example/mobility/MobilityApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mobility
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MobilityApplication
+
+fun main(args: Array<String>) {
+	runApplication<MobilityApplication>(*args)
+}
