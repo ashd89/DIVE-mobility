36ee02a856a295e8692c5dab20262d89f256e7de
ashd89 <jyjhlee0809@gmail.com>
2025-08-17 21:03:32 +0900

    feat: kotlin-logging 적용을 통한 URI logging구현

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,9 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
+	//kotlin-logging
+	implementation("io.github.oshai:kotlin-logging-jvm:7.0.12")
+
 	// Persistent (RDB / Cache)
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("org.postgresql:postgresql")
--- src/main/kotlin/com/example/mobility/kakao/dto/req/ClientRequest.kt
+++ src/main/kotlin/com/example/mobility/kakao/dto/req/ClientRequest.kt
@@ -9,7 +9,7 @@ data class ClientRequest (
     val priority: String? = "TIME", // 최소 시간 경로
     val avoid: String? = null, // 회피 루트
     val roadevent : Int? = 0, // 사고 반영
-    val alternate: Boolean? = true, // 다중 경로
+    val alternatives: Boolean? = true, // 다중 경로
     val road_details: Boolean? = true, // 도로 상세 정보
     val summary : Boolean? = false // 요약이 false야 나머지 추가 정보를 제공
 )
