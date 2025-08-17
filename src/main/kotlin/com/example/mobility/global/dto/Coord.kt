635446176efb84823cf3223e21104b69437a6125
ashd89 <jyjhlee0809@gmail.com>
2025-08-17 19:29:03 +0900

    feat: kakaoDirectionsApi 관련 DTO 작성

---
+++ src/main/kotlin/com/example/mobility/global/dto/Coord.kt
@@ -0,0 +1,6 @@
+package com.example.mobility.global.dto
+
+data class Coord(
+    val x: Double, // 경도
+    val y: Double  // 위도
+)
---
+++ src/main/kotlin/com/example/mobility/kakao/dto/req/ClientRequest.kt
@@ -0,0 +1,16 @@
+package com.example.mobility.kakao.dto.req
+
+import com.example.mobility.global.dto.Waypoint
+
+data class ClientRequest (
+    val origin: Waypoint, //출발지
+    val destination: Waypoint, //목적지
+    val waypoints: List<Waypoint>, // 경유지
+    val priority: String? = "TIME", // 최소 시간 경로
+    val avoid: String? = null, // 회피 루트
+    val roadevent : Int? = 0, // 사고 반영
+    val alternate: Boolean? = true, // 다중 경로
+    val road_details: Boolean? = true, // 도로 상세 정보
+    val summary : Boolean? = false // 요약이 false야 나머지 추가 정보를 제공
+)
+
